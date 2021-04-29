9bece4f4d405cb7135b220d0e871cd00cc9c1cd9
Mira Chami <miramc149@gmail.com>
2021-04-29 20:01:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Your code here
-function saturdayFun("roller-skate"){
-  return "This Saturday, I want to ${roller-skate} !"
+function saturdayFun(default="roller-skate"){
+  return " This Saturday, I want to ${default} !";
 }
